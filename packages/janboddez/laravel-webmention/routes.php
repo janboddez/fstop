914c2d4b017da05f612ed71a59fc7101db8f0c06
Jan Boddez <jan.boddez@gmail.com>
2024-10-26 16:40:11 +0200

    Submodules, hopefully

--- packages/janboddez/laravel-webmention/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use janboddez\Webmention\Http\Controllers\WebmentionController;
-
-Route::middleware('api')
-    ->post('/webmention', [WebmentionController::class, 'handle']);
