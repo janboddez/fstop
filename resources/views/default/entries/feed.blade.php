d850fdfc705d578fb1c09ab627fede6b3ad67a84
Jan Boddez <jan.boddez@gmail.com>
2024-11-09 12:37:53 +0100

    Use site title in feeds

--- resources/views/default/entries/feed.blade.php
+++ resources/views/default/entries/feed.blade.php
@@ -1,7 +1,7 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/">
 <channel>
-    <title>{{ config('app.name') }} {{ ! empty($type) ? ucfirst(Str::plural($type)) : __('Feed') }}</title>
+    <title>{{ site_name() }} &ndash; {{ ! empty($type) ? ucfirst(Str::plural($type)) : __('Feed') }}</title>
 
     {{-- <description>{{ __('On Web Development, WordPress, and More') }}</description> --}}
     <link>{{ url('/') }}</link>
