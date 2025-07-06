cc93879db0b8bba70f0e08c26532c1eeb41cc894
Jan Boddez <jan.boddez@gmail.com>
2025-07-06 11:32:08 +0200

    Fix published column migration

--- database/migrations/2025_07_03_000000_add_published_column_to_entries_table.php
+++ database/migrations/2025_07_03_000000_add_published_column_to_entries_table.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -16,6 +18,9 @@ return new class extends Migration
         Schema::table('entries', function (Blueprint $table) {
             $table->datetime('published')->nullable();
         });
+
+        DB::table('entries')->where('status', 'published')
+            ->update(['published' => DB::raw('`created_at`')]);
     }
 
     /**
@@ -26,7 +31,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('entries', function (Blueprint $table) {
-            dropConstrainedForeignId('published');
+            $table->dropColumn('published');
         });
     }
 };
